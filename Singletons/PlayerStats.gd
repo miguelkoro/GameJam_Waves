9c6613f335b7a6d0d50e1d2c99274fe9aa271768
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2025-11-20 20:34:04 +0100

    Modificacion del TileSet de la playa

--- Singletons/PlayerStats.gd
+++ Singletons/PlayerStats.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var health: float = 7 #Ponerle 7 corazones (del rollo gato de 7 vidas?)
 @export var max_health: float = 7 #Vida maxima 
-@export var agility: float = 100 #Velocidad a la que se mueve
+@export var agility: float = 120 #Velocidad a la que se mueve
 @export var strenght: float = 20 #Daño que hace a los enemigos (mas luego lo que haga el arma de daño)
 @export var endurance: float = 50 #Resistencia a los ataques
 
