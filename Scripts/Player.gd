5633ca1f9507d49fc39f1badcce828e2c4b1b37f
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2025-11-22 01:00:20 +0100

    Añadida corriente de agua Añadidos nuevos tilesets

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 var direction: Vector2 = Vector2.ZERO
 var attacking: bool = false
+var moving: bool = false
+var external_force: Vector2 = Vector2.ZERO #Esto lo uso para poder mover al personaje en aguas con corriente
 
 
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir.normalized() * PlayerStats.agility
+	if input_dir != Vector2.ZERO:
+		moving = true
+	else:
+		moving = false
+	#Añado la fuerza externa, si es afectado
+	var base_speed = input_dir.normalized() * PlayerStats.agility
+	#base_speed += external_force
+	#velocity += external_force
+	velocity = base_speed + external_force
+	
+	#velocity = input_dir.normalized() * PlayerStats.agility
 	move_and_slide()
+	
+	#Quito la fuerza externa para que no se acumule
+	external_force = external_force.move_toward(Vector2.ZERO, 100 * delta)
---
+++ Scripts/water_current.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var current_direction: Vector2 = Vector2.RIGHT
+@export var current_strength: float = 300.0
+
+var bodies_in_area: Array = []
+
+func _ready():
+	current_direction = current_direction.normalized()
+
+func _physics_process(delta):
+	for body in bodies_in_area:
+		#body.apply(current_direction * current_strength * delta)
+		#if body == CharacterBody2D:
+		if body is CharacterBody2D:
+			body.external_force += current_direction * current_strength * delta
+			
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body not in bodies_in_area:
+		bodies_in_area.append(body)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		body.external_force = Vector2.ZERO
+	bodies_in_area.erase(body)
