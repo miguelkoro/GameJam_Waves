07b5b961234af09616f071698fe8f9c6c2045adc
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2025-11-25 18:41:37 +0100

    Añadida interfaz de monedas/perlas Añadida fuente de pixel

--- Singletons/GameManager.gd
+++ Singletons/GameManager.gd
@@ -1,1 +1,12 @@
 extends Node
+
+var currency: float = 0 #Para guardar las perlas
+
+func add_currency(amount: float):
+	currency += amount
+	update_currency_gui()
+
+
+func update_currency_gui():
+	var gui = get_tree().get_first_node_in_group("PearlsUI")
+	#gui.update_hearts(last_health, health)
