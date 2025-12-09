957fe79a09966b30defc2c9bbe2f9e400aef7cd2
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2025-12-09 19:36:51 +0100

    Cambios en el menu de la tienda

--- Scripts/Map/shop.gd
+++ Scripts/Map/shop.gd
@@ -2,6 +2,12 @@ extends Node2D
 @onready var audio_Seagull: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var panel: Panel = $Panel
 var player_is_near: bool = false
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+var player = null
+var gui = null
+
+func _ready() -> void:
+	gui = get_tree().get_first_node_in_group("HeartsUI")
 
 func _process(delta: float) -> void:
 	if not player_is_near:
@@ -11,17 +17,31 @@ func _process(delta: float) -> void:
 			audio_Seagull.play()
 		#Aqui poner lo de abrir el menu de la tienda
 		print("Abrir tienda")
-		pass
+		canvas_layer.visible = true
+		if player != null:
+			player.inactive = true
+
+		gui.visible = false
+		
 
 
 func _on_show_label_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		panel.visible = true
+		player = body
 		player_is_near = true
+		
 		
 
 
 func _on_show_label_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		panel.visible = false
+		player = null
 		player_is_near = false
+
+
+func _on_back_button_pressed() -> void:
+	player.inactive = false
+	canvas_layer.visible = false
+	gui.visible = true
