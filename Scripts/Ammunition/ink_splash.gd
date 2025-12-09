fbf8ea299b53aaea5ca3da27a56429233f25ae63
Miguelkoro_PC\Miguelkoro <miguelkoro@gmail.com>
2025-12-09 22:46:29 +0100

    Modificado un poco el sprite de la tinta de calamar

---
+++ Scripts/Ammunition/ink_splash.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	queue_free()
--- Scripts/Ink.gd
+++ Scripts/Ink.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
-@export var speed: float = 250.0
+@export var speed: float = 180.0
 @export var lifetime: float = 3.0
+const INK_SPLASH = preload("uid://cfveuxic7ufkk")
+
 
 var direction: Vector2 = Vector2.ZERO
 var damage: float = 0.5
@@ -27,13 +29,19 @@ func _on_body_entered(body: Node2D) -> void:
 		if body.has_method("take_damage"):
 			body.take_damage(damage, global_position, knockback)
 			#Mancharse de tinta (Necesita que haya el ScreenEffects)
-			PlayerStats.screen_ink_effect() #Para darle efecto de mancharse de tinta
-
+			PlayerStats.screen_ink_effect() #Para darle efecto de mancharse de tinta	
+			ink_splash()	
 		# Pantalla negra
 		#if body.has_method("black_out_screen"):
 		#	body.black_out_screen()
 
 		queue_free()
 	elif body.is_in_group("World"):
+	#elif body.get_collision_layer() & (1 << 1): #Valor de World
 		# Choca con el escenario
+		ink_splash()
 		queue_free()
+func ink_splash() -> void:
+	var inkSplash = INK_SPLASH.instantiate()
+	inkSplash.global_position = global_position
+	get_parent().add_child(inkSplash)
